048aee6b30d8b7100a58af5f25c493b1ee3c1e62
Snoopyh <ismaelxd500@gmail.com>
2022-11-17 01:14:27 -0300

    Started Project Calculator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Calculator from './components/Calculator'
+import './App.css'
+
+function App() {
+ return (
+  <>
+  <h1>Calculadora</h1>
+  <Calculator/>
+  </>
+ )    
+}
+
+export default App
